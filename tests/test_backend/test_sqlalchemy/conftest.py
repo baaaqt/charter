01ebe0a6ab2d64378fb337c69617d7a8bc5b5e9a
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-07-28 01:18:53 +0700

    refactor: remove unused fixtures and config functions

--- tests/test_backend/test_sqlalchemy/conftest.py
+++ tests/test_backend/test_sqlalchemy/conftest.py
@@ -1,14 +1,6 @@
 from pathlib import Path
-from typing import Any
 
-import pytest
 from pytest import Config
-
-from charter._backends import load_backend
-
-
-def pytest_configure(config: Config):
-    config.addinivalue_line("markers", "sqlalchemy: mark test as sqlalchemy related")
 
 
 def pytest_ignore_collect(collection_path: Path, config: Config):
@@ -18,8 +10,3 @@ def pytest_ignore_collect(collection_path: Path, config: Config):
     except ImportError:
         skip = True
     return skip
-
-
-@pytest.fixture(scope="session")
-def backend() -> Any:
-    return load_backend("sqlalchemy")
